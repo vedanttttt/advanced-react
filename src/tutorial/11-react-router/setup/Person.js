b5c1cd626f381c402effd925eff2d1398b22eb4e
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2021-01-02 23:23:52 +0530

    react router

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  console.log(useParams());
+  const { id } = useParams();
+  const person = data.find((person) => person.id === parseInt(id));
+  console.log(person);
   return (
     <div>
-      <h2>person</h2>
+      <h1>{person.name}</h1>
+      <Link to="/people" className="btn">
+        Back to People
+      </Link>
     </div>
   );
 };
