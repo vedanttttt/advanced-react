43c76e8d6edd5c838dca4e55fc374254ed19504b
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2021-01-04 23:47:22 +0530

    React.memo , useCallback and useMemo

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,20 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setLoading(false);
-  };
+  }, [url]);
 
   useEffect(() => {
     getProducts();
-  }, [url]);
+  }, [url, getProducts]);
+
   console.log(products);
 
   return { loading, products };
 };
+
+//if we will simply add getProducts to dependancy array without using useCallback ,we will get an infinite loop bcoz then getProducts will get created each and everytime , now we only create the new function only once the url changes and as a result we are not getting the infinite loop
